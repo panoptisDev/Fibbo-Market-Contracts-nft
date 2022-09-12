519d44074d29f19ddb0219e3c6bba9cc1880758e
antonicarol <antonicarolgit@gmail.com>
2022-09-12 20:46:03 +0200

    prepared

--- scripts/utils/verifyArtist.js
+++ scripts/utils/verifyArtist.js
@@ -19,7 +19,7 @@ async function main(network) {
     "FibboVerification",
     verificationAddress
   );
-  /* 
+
   await verification.verificateAddress(
     "0x1d92D9a839e9c5D8cc02A7F87E591fF1AdA33268"
   );
@@ -34,27 +34,6 @@ async function main(network) {
   );
   await verification.verificateAddress(
     "0x3a8FDCa22221CC7C67F95520466b1E991B49168d"
-  ); */
-  await verification.verificateAddress(
-    "0x1d92D9a839e9c5D8cc02A7F87E591fF1AdA33268"
-  );
-  await verification.verificateAddress(
-    "0x06b3cC29D74a36f15F1B2beD529Fe45E30CAaf12"
-  );
-  await verification.verificateAddress(
-    "0x41162C850B7176CA0A4310c33F81ffB2E0dCf99F"
-  );
-  await verification.verificateAddress(
-    "0x1d5318c25AcDCc270b3f92CcDB72d245836bBCc1"
-  );
-  await verification.verificateAddress(
-    "0xDE0D0F1548B16036F6bd9cE759A32D828fF3f15d"
-  );
-  await verification.verificateAddress(
-    "0x07d953152a282FC6F7D56Ef43e680065807EbEe7"
-  );
-  await verification.verificateAddress(
-    "0xe3DD5007b8ceb22573965BbEB16eE790b772CB45"
   );
 }
 
